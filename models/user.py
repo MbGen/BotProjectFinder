076dc5c939dfd4efe2875c05ccefb24ed01832e1
MbGen <redpyl6@gmail.com>
2021-12-20 16:30:37 +0200

    модель User (updated)

--- models/user.py
+++ models/user.py
@@ -1,22 +1,19 @@
 from utils.db_api import db
 from peewee import *
 from datetime import datetime
-from enum import Enum
-
-
-class TypeOfUser(Enum):
-    searcher = 1
-    creator = 2
 
 
 class User(Model):
-    id = IntegerField(primary_key=True)
-    nickname = CharField()
-    age = IntegerField()
-    theme = CharField()
-    skills = CharField()
-    about = CharField()
-    type_of_user: TypeOfUser = BitField(db_column="typeOfUser")
+    id = IntegerField(primary_key=True, null=False)
+    nickname = CharField(default=None)
+    age = IntegerField(default=None)
+    theme = CharField(default=None)
+    skills = CharField(default=None)
+    about = CharField(default=None, null=True)
+    type_of_user = BooleanField(db_column="typeOfUser")
+
+    is_searcher = type_of_user.flag(0)
+    is_creator = type_of_user.flag(1)
 
     class Meta:
         database = db
