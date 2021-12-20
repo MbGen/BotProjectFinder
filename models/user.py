f18d41be82eee16b6084834ef5183e4bbf1f4d3c
george <test@localhost>
2021-12-20 17:00:38 +0300

    Create user model

---
+++ models/user.py
@@ -0,0 +1,11 @@
+from utils.db_api import db
+from peewee import *
+from datetime import datetime
+
+
+class User(Model):
+    id = IntegerField(primary_key=True)
+    created_at = DateTimeField(default=datetime.now)
+
+    class Meta:
+        database = db
