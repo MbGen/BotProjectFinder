83385f0f4db028fcf092b1653ecc191560d21117
george <test@localhost>
2021-12-24 21:15:48 +0300

    выход из state при /start

--- handlers/users/authorization/start.py
+++ handlers/users/authorization/start.py
@@ -2,12 +2,14 @@ from aiogram import types, filters
 from aiogram.dispatcher.filters.builtin import CommandStart
 from models.user import User
 from loader import dp
+from aiogram.dispatcher import FSMContext
 from keyboards import inline
 
 keyboard_auth = inline.callbacks.AuthorizationKB.authorize_kb()
 
 
-@dp.message_handler(CommandStart(), filters.ChatTypeFilter(types.ChatType.PRIVATE))
-async def bot_start(msg: types.Message) -> None:
+@dp.message_handler(CommandStart(), filters.ChatTypeFilter(types.ChatType.PRIVATE), state='*')
+async def bot_start(msg: types.Message, state: FSMContext) -> None:
     User.get_or_create(id=msg.from_user.id)
+    await state.finish()
     await msg.answer(f"👋 Привет, {msg.from_user.first_name}", reply_markup=keyboard_auth)
