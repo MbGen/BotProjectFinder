6cc7cddf9181f7897dc17f84e22d6f48997335da
MbGen <redpyl6@gmail.com>
2021-12-24 17:50:51 +0200

    Revert "Revert "Добавил постепенную дозапись данных в бд.""
    This reverts commit 7701e44b9e633cc2f299ad38c0a16a4099c0d6a9.

--- handlers/users/authorization/nickname.py
+++ handlers/users/authorization/nickname.py
@@ -3,6 +3,7 @@ from loader import dp
 from aiogram.dispatcher import FSMContext
 from states.user.authorization import Authorization
 from utils.validation.data_validation import Validator
+from models.user import User
 
 
 @dp.message_handler(state=Authorization.waiting_for_nickname)
@@ -10,6 +11,9 @@ async def get_nickname(msg: types.Message, state: FSMContext) -> None:
     if Validator.is_valid_nickname(msg.text):
         await state.update_data(nickname=msg.text)
         await msg.answer(f"Отлично, теперь ваш возраст (можете соврать)")
+        user_cursor = User.get(User.id == msg.from_user.id) 
+        user_cursor.nickname = msg.text 
+        user_cursor.save()
         await Authorization.waiting_for_age.set()
     else:
         await msg.answer("Никнейм занят, введите заново")
