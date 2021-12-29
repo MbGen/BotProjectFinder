82b98d62b497ac2518d7ed048322d3739253943a
MbGen <redpyl6@gmail.com>
2021-12-29 15:41:13 +0200

    Добавил навигацию по меню без новых сообщений

--- handlers/users/menu/main_menu.py
+++ handlers/users/menu/main_menu.py
@@ -1,7 +1,9 @@
-from loader import dp
+from loader import dp, bot
 from aiogram import types, filters
 from keyboards import inline
 from keyboards.inline.callback_data import MenuCallback
+from typing import Union
+
 
 keyboard_menu = inline.callbacks.MenuKB.get_menu_kb()
 
@@ -9,3 +11,8 @@ keyboard_menu = inline.callbacks.MenuKB.get_menu_kb()
 @dp.message_handler(filters.ChatTypeFilter(types.ChatType.PRIVATE), commands=["menu"])
 async def main_menu(msg: types.Message) -> None:
     await msg.answer("Вот что вы можете сделать", reply_markup=keyboard_menu)
+
+
+@dp.callback_query_handler(text_contains=MenuCallback.MAIN_MENU)
+async def main_menu_callback(callback_query: types.CallbackQuery) -> None:
+    await callback_query.message.edit_text(text="Вот что вы можете сделать", reply_markup=keyboard_menu)
--- handlers/users/profile/profile.py
+++ handlers/users/profile/profile.py
@@ -9,5 +9,4 @@ keyboard_profile = inline.callbacks.MenuKB.get_profile_kb()
 
 @dp.callback_query_handler(text_contains=MenuCallback.PROFILE)
 async def profile(callback_query: types.CallbackQuery) -> None:
-    # TODO: Вместо новго сообщения, изменять старое
-    await bot.send_message(callback_query.from_user.id, "Ваш профиль", reply_markup=keyboard_profile)
+    await callback_query.message.edit_text(text="Ваш профиль", reply_markup=keyboard_profile)
