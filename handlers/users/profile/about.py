30a9663a74fa3b8e267b5f51769530872d883d96
MbGen <redpyl6@gmail.com>
2022-01-03 18:01:08 +0200

    Дописал кнопку about_me

--- handlers/users/profile/about.py
+++ handlers/users/profile/about.py
@@ -1,9 +1,21 @@
 from loader import dp, bot
 from aiogram import types, filters
+import aiogram.utils.markdown as fmt
 from keyboards import inline
 from keyboards.inline.callback_data import ProfileCallback
+from models.user import User
 
 
 @dp.callback_query_handler(text_contains=ProfileCallback.ABOUT_ME)
-async def about_me(callback_query: types.CallbackQuery) -> None:\
-    await bot.send_message(callback_query.from_user.id, text="Информация об юзере")
+async def about_me(callback_query: types.CallbackQuery) -> None:
+    user_cursor = User.get(id=callback_query.from_user.id)
+    await bot.send_message(callback_query.from_user.id,
+        fmt.text(
+            fmt.code("Ваш никнейм - ", user_cursor.nickname),
+            fmt.code("Ваш возраст - ", user_cursor.age),
+            fmt.code("Ваша выбранная тема - ", user_cursor.theme),
+            fmt.code(f"Что вы умеете - ", user_cursor.skills),
+            fmt.code(f"О вас - ", user_cursor.about if user_cursor.about else 'Вы ничего о себе не писали'),
+            sep="\n"
+        )
+    )
--- loader.py
+++ loader.py
@@ -5,6 +5,6 @@ from os import getenv
 
 load_dotenv()
 
-bot = Bot(token=getenv('botToken'), parse_mode=types.ParseMode.HTML)
+bot = Bot(token=getenv('botToken'), parse_mode=types.ParseMode.MARKDOWN_V2)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
